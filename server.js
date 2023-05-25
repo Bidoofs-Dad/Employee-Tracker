92fa38fbfc7cbc1929acc33e4dda1a5daac1ddf0
Zane Werle <zanewerle@gmail.com>
2023-05-25 14:23:36 -0500

    updated the view all employees function to add the extra stuff I initially missed, also created the functions to add new roles and departments

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const navigateChoices = () => {
             'Add A Role',
             'Add An Employee',
             'Update An Employee Role'],
-    // this will deploy a function based off of what choice you make, these are placeholder names as I havent written the functions yet
+        // this will deploy a function based off of what choice you make, these are placeholder names as I havent written the functions yet
     }).then(answer => {
         switch (answer.navigate) {
             case 'View All Departments':
@@ -65,22 +65,78 @@ const viewAllDepartments = () => {
     db.query('SELECT * FROM department;', function (err, results) {
         console.table(results);
         navigateChoices();
-      });
+    });
 }
 
 const viewAllRoles = () => {
     db.query('SELECT * FROM role;', function (err, results) {
         console.table(results);
         navigateChoices();
-      });
+    });
 }
 
 const viewAllEmployees = () => {
-    db.query('SELECT * FROM employee;', function (err, results) {
+    db.query('SELECT employee.id, first_name, last_name, role.title, department.name, role.salary, manager_id FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id;', function (err, results) {
         console.table(results);
         navigateChoices();
-      });
+    });
 }
+
+const addADepartment = () => {
+    inquirer.prompt({
+        type: "input",
+        name: "newDepartment",
+        message: "What would you like to name your new department?"
+
+    }).then((answer) => {
+        db.query("INSERT INTO department (name) VALUES (?)", [answer.newDepartment], (err, results) => {
+            db.query("SELECT * FROM department", (err, results) => {
+                console.table(results);
+                navigateChoices();
+            })
+        })
+    })
+};
+
+const addARole = () => {
+    inquirer.prompt([{
+        type: "input",
+        name: "jobTitle",
+        message: "What would you like to name your new job title?"
+    },
+    {
+        type: "number",
+        name: "salary",
+        message: "How much does this new position pay?"
+    },
+    {
+        type: "number",
+        name: "departmentID",
+        message: "What is the Department ID associated with this new position?"
+    }
+    ]).then((answer) => {
+        db.query("INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)", [answer.jobTitle, answer.salary, answer.departmentID], (err, results) => {
+            db.query("SELECT * FROM role", (err, results) => {
+                console.table(results);
+                navigateChoices();
+            })
+        })
+    })
+};
+
+// const addAnEmployee = () => {
+//     db.query('SELECT * FROM role;', function (err, results) {
+//         console.table(results);
+//         navigateChoices();
+//     });
+// }
+
+// const updateAnEmployeeRole = () => {
+//     db.query('SELECT * FROM role;', function (err, results) {
+//         console.table(results);
+//         navigateChoices();
+//     });
+// }
 
 
 navigateChoices();
